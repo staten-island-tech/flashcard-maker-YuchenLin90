cb7c594afd060fa7c753628b7b5c67ed6caa7172
YuchenLin90 <YUchenl27@nycstudents.net>
2025-04-09 10:14:11 -0400

    commit

---
+++ app.py
@@ -0,0 +1,20 @@
+A = []
+R = 5
+N = 1
+P = 750
+D = -1
+
+while True:
+    N = N * R
+    A.append(N)
+    D =D +1 
+    for i in A:
+        + i
+    # print(f"{A}")
+    if i > P:
+        break
+print(f"The number of people being infected is {A} in day {D}")
+        
+
+        
+       
